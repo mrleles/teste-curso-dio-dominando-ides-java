66219c3e78fd204665aa3cdb977e30a10df98919
Marlos Leles <marlosleles@gmail.com>
2022-10-06 09:23:27 -0300

    Implementing the calculator

--- src/hello_world/FirstClass.java
+++ src/hello_world/FirstClass.java
@@ -1,12 +1,20 @@
 package hello_world;
 
+import java.util.Scanner;
+
 public class FirstClass {
 
 	public static void main(String[] args) {
-		int a = 2;
-		int b = 3;
-		System.out.println("Hello World = " + (a + b));
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Primeiro número:");
+		int a = scan.nextInt();
+		System.out.println("Primeiro número:");
+		int b = scan.nextInt();
+		
+		System.out.println("Soma: " + (a + b));
+		System.out.println("Subtração: " + (a - b));
+		System.out.println("Divisão: " + (a / b));
+		System.out.println("Multiplicação: " + (a * b));
 
 	}
-
 }
