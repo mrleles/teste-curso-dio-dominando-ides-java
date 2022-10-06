4e023f763277b34c083050e355e4579f86f80171
Marlos Leles <marlosleles@gmail.com>
2022-10-06 08:26:57 -0300

    First Commit

---
+++ src/hello_world/FirstClass.java
@@ -0,0 +1,12 @@
+package hello_world;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello World = " + (a + b));
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author leles
+ *
+ */
+module hello_world {
+}
